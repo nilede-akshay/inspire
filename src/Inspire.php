29ad4dcc064ed5c3f8b277705e498d47eecd17bc
devakshaythorat <akshay@nilede.tech>
2024-10-16 17:41:21 +0530

    Initial commit

---
+++ src/Inspire.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace NiledeAkshay\DemoPackage;
+
+use Illuminate\Support\Facades\Http;
+
+class Inspire {
+    public function justDoIt() {
+        return "The will of man is his happiness.";  
+    }
+}
---
+++ src/Providers/InspirationProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace NiledeAkshay\DemoPackage\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class InspirationProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/../views', 'inspire');
+    }
+}
